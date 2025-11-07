ebce2f512ad979ec6538721db1f4a5f2a65e85d4
Guillaume G <guillaume.gerard2112@outlook.fr>
2025-11-07 10:13:35 +0100

    fix : update migration file

--- database/migrations/2025_11_04_091353_create_difficulty_table.php
+++ database/migrations/2025_11_04_091353_create_difficulty_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('difficulty', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->smallInteger('point')->unique();
+            $table->smallInteger('point');
             $table->timestamps();
         });
 
