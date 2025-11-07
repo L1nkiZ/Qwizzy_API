1174b922551d2cc96edcf472f00bf25ada1ac553
unknown <Nytrofox960@gmail.com>
2025-11-07 10:58:32 +0100

    feat: Add token auth on /user route part 2

---
+++ app/Http/Middleware/CheckAuthToken.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAuthToken
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->header('Authorization');
+
+        $validToken = env('AUTH_TOKEN');
+
+        if (!$token || $token !== $validToken) {
+            return response()->json(['message' => 'Token invalide ou manquant'], 401);
+        }
+
+        return $next($request);
+    }
+}
